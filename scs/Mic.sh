d82e2c32364d0ac40839ddd3836a09c9d54fb626
YSW0630 <justin.sw.yang@gmail.com>
2024-08-08 21:38:23 +0800

    Add right click and left click function of Mic.sh

--- scs/Mic.sh
+++ scs/Mic.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 case $BUTTON in
-  2) amixer set Capture toggle ;;
+  1) amixer set Capture toggle ;;
+  2) pavucontrol --tab=4 ;;
+  3) alacritty -e alsamixer -V capture ;;
 esac
 
+# amixer get Capture | rg "\[[0-9]+%\]"
 amixer get Capture | rg -q "\[off\]" # return 1: mic on, 0: mic off
 
 if [ "$?" == 1 ]; then
