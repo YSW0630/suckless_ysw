25574cd0909be83ca4fc87fb782ed5bce84b85ad
YSW0630 <justin.sw.yang@gmail.com>
2024-08-25 16:33:25 +0800

    move dwm and dwmblocks out

--- blocks_colored/blocks.h
+++ blocks_colored/blocks.h
@@ -2,17 +2,17 @@
 // recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {"^c#5A99F7^   ", "$HOME/scs/Kernel.sh", 0, 1},
+    {"^c#5A99F7^  ", "$HOME/scs/Kernel.sh", 0, 1},
     // {  ",  "$HOME/scs/Wifi.sh",      5,	  	2},
     // {󰈀  ",  "$HOME/scs/Ethernet.sh",  5,	  	3},
-    {"^c#98c379^   ", "$HOME/scs/Volume.sh", 0, 4},
-    {"", "$HOME/scs/Mic.sh", 0, 11},
-    {"^c#E8B94D^ 󰛨  ", "$HOME/scs/Light.sh", 0, 5},
+    {"^c#98c379^  ", "$HOME/scs/Volume.sh", 0, 4},
+    {"",    "$HOME/scs/Mic.sh", 0, 11},
+    {"^c#E8B94D^ 󰛨 ", "$HOME/scs/Light.sh", 0, 5},
     // {"^c#F24F4F^   ", "$HOME/scs/Update.sh", 1800, 6},
     // {󰠠  ", "$HOME/scs/Battery.sh",    10,  	7},
-    {"^c#F24F4F^ 󰍛  ", "$HOME/scs/Memory.sh", 5, 8},
-    {"^c#DDADF6^ 󰋊  ", "$HOME/scs/Disk.sh", 1800, 9},
-    {"^c#5A99F7^   ", "$HOME/scs/Clock.sh", 1, 10},
+    {"^c#F24F4F^ 󰍛 ", "$HOME/scs/Memory.sh", 5, 8},
+    {"^c#bb5eed^ 󰋊 ", "$HOME/scs/Disk.sh", 1800, 9},
+    {"^c#5A99F7^  ", "$HOME/scs/Clock.sh", 1, 10},
 };
 
 // sets delimiter between status commands. NULL character ('\0') means no
