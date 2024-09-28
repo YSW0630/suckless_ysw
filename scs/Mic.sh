0e82013eb10651005bb8cd250e4c7fbb5e1ffef5
YSW0630 <justin.sw.yang@gmail.com>
2024-09-28 10:33:28 +0800

    add on and off to Mic.sh

--- scs/Mic.sh
+++ scs/Mic.sh
@@ -10,7 +10,7 @@ esac
 amixer get Capture | rg -q "\[off\]" # return 1: mic on, 0: mic off
 
 if [ "$?" == 1 ]; then
-  echo "^c#98c379^ "
+  echo "  on"
 else 
-  echo "^c#98c379^ "
+  echo "  off"
 fi
