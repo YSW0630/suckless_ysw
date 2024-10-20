7e028f6a4e076df6cac9fe62571e983197bb7973
Pacfrog <95664509+YSW0630@users.noreply.github.com>
2024-10-20 13:52:51 +0800

    Update config.def.h
    Add 6~9 tags to dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -37,7 +37,7 @@ static const char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5"};
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 // static const char *tags[] = { "", "", "", "", "󰇮", "", ""};
 // static const char *tags[] = { "󰎤", "󰎧", "󰎪", "󰎭", "󰎱"};
 // static const char *tags[] = { "Home", "Web", "Code", "Pcman", "Vbox", "Misc"};
